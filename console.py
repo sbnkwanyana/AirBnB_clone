43334e9909ecd9e06d7c5712f18ae4bae3a949b6
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-10-30 15:50:55 +0200

    initial console with pycodestyle tests validates

--- console.py
+++ console.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 """
-Console.py - 
+Console.py - contains the interactive/non-interactive shell class
+that is the entry point of the console application
 """
 
 import cmd
 import shlex
 
+
 class Shell(cmd.Cmd):
+    """
+    Shell class prompts and accepts input from the a terminal and interprets
+    the commands and runs the corresponding command
+    """
+
     prompt = "(hbnb) "
 
     def do_EOF(self, arg):
-        "End of file: Exit"
+        "End of file and quits"
         exit()
-    
+
     def do_quit(self, arg):
-        'Quit program'
+        "Quits program"
         exit()
+
 
 if __name__ == "__main__":
     Shell().cmdloop()
