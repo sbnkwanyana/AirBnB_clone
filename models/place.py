523d39ccb2f0e2f4375629889f74c393d5295a8d
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-10-31 23:23:26 +0200

    longitude and latitude values to float

--- models/place.py
+++ models/place.py
@@ -11,7 +11,7 @@ class Place(BaseModel):
     """
     class holds a places city_id, user_id, name, description
     number_rooms, nuber_bathrooms, max_guest, price_by_night
-    latitude, longitude and list of amenities_ids
+    latitude, longitude and list of amenity_ids
     """
     city_id = ""
     user_id = ""
@@ -21,6 +21,6 @@ class Place(BaseModel):
     number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
-    latitude = 0
-    longitude = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = []
