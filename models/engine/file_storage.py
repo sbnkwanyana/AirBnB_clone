8d63255c9a167d32797c4ea6b3a0ceef38156677
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-10-31 23:30:49 +0200

    added import statements

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,13 @@ This module contains the file storage engine of the program
 """
 
 import json
-from models.base_model import *
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class FileStorage():
